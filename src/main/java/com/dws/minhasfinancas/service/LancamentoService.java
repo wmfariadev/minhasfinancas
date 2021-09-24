9f8649effc0ead6b27061a595c01f1e6544a810d
Willian Faria <wmfarinha@gmail.com>
2021-09-24 12:03:57 -0300

    Serviço de lançamento

---
+++ src/main/java/com/dws/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,21 @@
+package com.dws.minhasfinancas.service;
+
+import com.dws.minhasfinancas.model.entity.Lancamento;
+import com.dws.minhasfinancas.model.enums.StatusLancamento;
+
+import java.util.List;
+
+public interface LancamentoService {
+
+    Lancamento salvar(Lancamento lancamento);
+
+    Lancamento atualizar(Lancamento lancamento);
+
+    void deletar(Lancamento lancamento);
+
+    List<Lancamento> buscar(Lancamento lancamentoFiltro);
+
+    void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+
+    void validar(Lancamento lancamento);
+}
---
+++ src/main/java/com/dws/minhasfinancas/service/impl/LancamentoServiceImp.java
@@ -0,0 +1,84 @@
+package com.dws.minhasfinancas.service.impl;
+
+import com.dws.minhasfinancas.exception.RegraNegocioException;
+import com.dws.minhasfinancas.model.entity.Lancamento;
+import com.dws.minhasfinancas.model.enums.StatusLancamento;
+import com.dws.minhasfinancas.model.repository.LancamentoRepository;
+import com.dws.minhasfinancas.service.LancamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class LancamentoServiceImp implements LancamentoService {
+
+    @Autowired
+    LancamentoRepository repository;
+
+    @Override
+    @Transactional
+    public Lancamento salvar(Lancamento lancamento) {
+        validar(lancamento);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
+        return repository.save(lancamento);
+    }
+
+    @Override
+    @Transactional
+    public Lancamento atualizar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
+        return repository.save(lancamento);
+    }
+
+    @Override
+    @Transactional
+    public void deletar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        repository.delete(lancamento);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+        Example example = Example.of(lancamentoFiltro, ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        return repository.findAll(example);
+    }
+
+    @Override
+    @Transactional
+    public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+        lancamento.setStatus(status);
+        atualizar(lancamento);
+    }
+
+    @Override
+    public void validar(Lancamento lancamento) {
+        if (lancamento.getDescricao() == null || lancamento.getDescricao().trim() == "")
+            throw new RegraNegocioException("Descrição não pode ser vazia");
+
+        if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12)
+            throw new RegraNegocioException("Informe um mês válido");
+
+        if (lancamento.getAno() == null || lancamento.getAno().toString().length() < 4)
+            throw new RegraNegocioException("Informe um ano válido");
+
+        if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null)
+            throw new RegraNegocioException("Informe um usuário");
+
+        if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1)
+            throw new RegraNegocioException("Informe um valor válido");
+
+        if (lancamento.getTipo() == null) throw new RegraNegocioException("Informe um tipo de lançamento");
+    }
+}
